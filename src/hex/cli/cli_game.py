ddc73e8452ae1f45e2aff39612248929483f4f3a
Daniel Moore <xdhmoore@gmail.com>
2025-06-22 23:46:14 +0000

    feat(movement): replace cli game with better code from demo grid

--- src/hex/cli/cli_game.py
+++ src/hex/cli/cli_game.py
@@ -1,32 +1,23 @@
+
+
 import logging
-import re
 from dataclasses import dataclass
-from functools import reduce
-from inspect import cleandoc
-from pprint import pprint
-from typing import Dict, List
+from time import sleep
+from typing import Dict, OrderedDict, cast
 
 import blessed
 from blessed import Terminal
-from blessed.keyboard import Keystroke
+from blessed.keyboard import Keystroke, resolve_sequence
 
 from hex.board import Board
-from hex.cli.keys import Keys
 from hex.cli.screen_manager import ScreenManager
+from hex.cli.templates.template import Template
+from hex.cli.utils import new_keystroke
 from hex.piece import Piece
+from hex.piece_type import PieceType
+from hex.player import Player
+from hex.position import Position
 
-# TODO
-"""
-- place
-- display text
-- move, single player
-- setup REST api
-- make cli client
-- make browser client
-
-
-# TODO configure this in file shared across main and demos
-"""
 logging.basicConfig(
     filename="./logs/hex.log",
     encoding="utf-8",
@@ -37,11 +28,46 @@ logging.basicConfig(
     level=logging.DEBUG,
 )
 
-# TODO mypy
-# TODO store game board and operations as numpy array so tensorflow or pytorch can eval fast
+
+@dataclass
+class Box:
+    min_x: int
+    min_y: int
+
+    max_x: int
+    max_y: int
+
+# TODO use numpy for thse
+
+
+def term_bounding_box(term: Terminal) -> Box:
+    # RESUME
+    return Box(0, 0, term.width - 1, term.height - 1)
+
+
+def term_to_hex_box(box: Box) -> Box:
+    num_vert: int = (box.max_y - box.min_y) // Template.HEIGHT
+    num_horiz: int = (box.max_x - box.min_x) // Template.WIDTH
+    if (num_vert % 2 == 0):
+        num_above_orig = (num_vert // 2) - 1
+    else:
+        num_above_orig = num_vert // 2
+    # TODO
+    return None
+
+
+def generate_place_pieces(board: Board):
+    # Calc equivalent hex coords box with 0,0,0 in the middle
+    for a in range(2):
+        for r in range(10):
+            for c in range(10):
+                piece = Piece(Position(a, c, r),
+                              PieceType.NoPiece, Player.Player1)
+                board.move(piece, Position(a, c, r))
 
 
 class CliGame:
+
     # System Seq Diagram : [MermaidChart: 1d3677c8-35c2-4a64-9971-d59d7e11e9bd]
     # Seq Diagram: [MermaidChart: 0cc01e70-aa53-4810-889d-46c95a7dcfb3]
     def main(self) -> None:
@@ -52,54 +78,64 @@ class CliGame:
             with term.fullscreen(), term.cbreak(), term.hidden_cursor():
                 print(term.home + term.clear)
                 board = Board()
+                mgr = ScreenManager(
+                    board, term, debug=True, slow_display=False)
 
-                mgr = ScreenManager(board, term)
-                first = True
+                # Calc term box size
 
-                # piece1 = Piece(None)
-                # board.move(piece1, Position(0, 0, 0))
-                # TODO try these 3 next:
-                # piece1 = Piece(None, PieceType.Queen)
-                # board.move(piece1, Position(0, 2, -1))
-                # piece2 = Piece(None, PieceType.Ant)
-                # board.move(piece2, Position(1, 1, 1))
-                # piece3 = Piece(None, PieceType.Grasshopper)
-                # board.move(piece3, Position(0, 0, -1))
-                key: Keystroke = term.inkey(timeout=0.1)
-                # shouldn't what is passed around be type Keystroke
-                # https://blessed.readthedocs.io/en/latest/api/keyboard.html#blessed.keyboard.Keystroke
-                while not key is None and key.name != "q":  # and key != '':
-                    # TODO - best I can tell, inkey() only returns an obj with
-                    # a code or a name if it's a non-alphanumeric key, ie an "application key"
-                    # arrow keys are with in this, but I will probably want to use reg keys
-                    # later
-                    try:
-                        # if key != None:
-                        # TODO implement player turns
-                        # TODO move term to __init__(term)
-                        # if key and key.code in Keys.arrows(term):
-                        #     #logging.debug('key is', key.code)
-                        #     logging.debug('should move')
-                        #     mgr.move_selector(key)
-                        #     # mgr.selector.activate()
-                        if (key):
-                            logging.debug("acting on key " + (key.name or ""))
-                            mgr.onkey(key)
+                # term_box = term_bounding_box(term)
+                generate_place_pieces(board)
+                # TODO also, commit per day
 
-                        mgr.display()
-                        key = term.inkey(timeout=5.5)
-                    except Exception as ex:
-                        logging.error(ex)
-                        if (key):
-                            logging.debug('%i', key.code)
-                        else:
-                            logging.debug('None')
-                        raise ex
+                # first = True
+                # while True:
+                #     inkeys = []
+                #     if (first):
+                #         process_and_display(term, mgr, [], True)
+                #         first = False
+                #         continue
+
+                #     has_input = term.kbhit(0.2)
+                #     while has_input:
+                #         next_inch: str = term.getch()
+                #         if next_inch:
+                #             inkeys = inkeys + [next_inch]
+                #             has_input = term.kbhit(0)
+                #         else:
+                #             has_input = False
+                # process_and_display(term, mgr, inkeys, first)
+                val = term.inkey(0.1)
+                process_and_display(term, mgr, val, force=True)
+                while not val is None and val != "q":
+                    val = term.inkey(0.1)
+                    process_and_display(term, mgr, val)
+
+                    # logging.debug(inkeys)
+                    # TODO limit fps using SDL_delay like pygame tick or other delay function, sleep()?
+                    # https://www.pygame.org/docs/ref/time.html#pygame.time.Clock.tick
+                    # https://github.com/libsdl-org/SDL/blob/aaa5d70efcd48ab0dd6759ac18964333c8c1a95d/src/timer/windows/SDL_systimer.c#L106
+                    # https://github.com/libsdl-org/SDL/blob/aaa5d70efcd48ab0dd6759ac18964333c8c1a95d/src/timer/unix/SDL_systimer.c#L138
+
+                # Loop through hex coords and place a new piece on the board at each
+
+                # alter screen cells so that they print the hex coords
+                # add a key shortcut to switch with screen space coords, before translation
+                # maybe shortcut to show screen space coords after translation too
         except Exception as ex:
-            logging.error("Error", exc_info=ex)
-            exit()
+            logging.error(msg=ex)
+            exit(1)
 
-            # x = input()
+
+def process_and_display(term: Terminal, mgr: ScreenManager, ks, force=False) -> None:
+    actions_taken = process_keystrokes(term, mgr, ks)
+    if actions_taken or force:
+        mgr.display()
+
+
+def process_keystrokes(term: Terminal, mgr: ScreenManager, ks) -> bool:
+    actions_taken: bool = False
+    actions_taken = actions_taken or mgr.onkey(ks)
+    return actions_taken
 
 
 if __name__ == "__main__":
